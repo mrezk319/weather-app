204f2bbbffb4e81e8e833ec051f098a5dfa2d108
Muhammed Rezk Rajab <mrezk319@gmail.com>
2025-03-15 17:57:49 +0200

    edits

--- lib/weather/cubit/weather_cubit.dart
+++ lib/weather/cubit/weather_cubit.dart
@@ -86,9 +86,6 @@ class WeatherCubit extends Cubit<WeatherState> {
     bool serviceEnabled;
     LocationPermission permission;
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    // if (!serviceEnabled) {
-    //   return Future.error('Location services are disabled.');
-    // }
     permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
